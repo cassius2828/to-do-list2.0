66e23a2e94976ff090dd6e992908b5c22a25d071
Cassius Reynolds <124099580+cassius2828@users.noreply.github.com>
2023-04-04 20:18:29 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,82 @@
-
 // global vars
 
 const button = document.getElementById("enter");
 const input = document.getElementById("user-input");
 const ul = document.querySelector("ul");
 const clear = document.getElementById("clear");
+const icons = document.querySelector("i");
+const listItems = document.querySelectorAll("li");
+const pencil = new Audio('sounds/pencil-sound.wav');
 
 // functions
 
 function inputLength() {
-   return input.value.length;
+  return input.value.length;
 }
 
 function addListItem() {
-    const li = document.createElement("li");
-li.appendChild(document.createTextNode(input.value));
-ul.appendChild(li);
-input.value = "";
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  li.addEventListener("click", markList);
+
+  const icon = document.createElement("i");
+  icon.classList.add("fa-solid", "fa-trash-can");
+  li.appendChild(icon);
+  icon.addEventListener("click", () => li.remove());
+
+  ul.appendChild(li);
+  input.value = "";
 }
 
-function addListAfterClick() {
-    if (inputLength() > 0 && input.value != " " && input.value != "  ") {
-        addListItem();
-      }
-}
+// arrow function version
+const addListAfterClick = () => {
+  if (inputLength() > 0 && input.value != " " && input.value != "  ") {
+    addListItem();
+  }
+};
 
 function addListAfterKeypress(event) {
-    if (inputLength() > 0 && input.value != " " && input.value != "  " && event.which === 13 ) {
-        addListItem();
-      }
+  if (
+    inputLength() > 0 &&
+    input.value != " " &&
+    input.value != "  " &&
+    event.which === 13
+  ) {
+    addListItem();
+  }
 }
 
 function markList(e) {
-    if (e.target) {
-        e.target.classList.toggle("done");
-    }
+  if (e.target) {
+    e.target.classList.toggle("done");
+  }
 }
 
 function clearContent() {
-    ul.innerHTML = "";
+  ul.innerHTML = "";
 }
 
+function clearTask(e) {
+  const li = document.querySelector("li");
+  li.parentNode.removeChild(li);
+}
 
+function clearTask() {
+  listItems.parentNode.removeChild(listItems);
+}
 
 // running code
-
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
-ul.addEventListener("click", markList);
-
 clear.addEventListener("click", clearContent);
+
+// listItems = [1, 2, 3, 4, 5];
+//
+// for (i=0; i < li.length; i++) {
+// listItems[i].addEventListener('click', 'do something')
+// }
+//
+// for loop rewritten
+// listItems.forEach(item => item.addEventListener('click') )
